9abe52acc05481898d7a5518e3ca86fd08a47864
Chayut <chun.chayut@gmail.com>
2025-09-05 09:20:34 +0700

    Fix desktop layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="h-[100dvh] overflow-hidden bg-secondary">
       <div className="mx-auto h-full max-w-[528px] p-4 lg:max-w-7xl">
-        <div className="flex h-full flex-col gap-4 lg:flex-row lg:gap-4">
+        <div className="flex h-full flex-col gap-4 lg:flex-row lg:justify-center lg:gap-4">
           <Display />
-          <div className="flex-1 overflow-hidden lg:max-w-[420px]">
+          <div className="overflow-hidden lg:min-w-[420px]">
             <Controls />
           </div>
         </div>
